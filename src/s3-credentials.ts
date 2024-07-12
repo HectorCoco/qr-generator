175dbd41128ca8ad3eafada33b73bbd9c6aae8aa
Hector Alpuche Rodriguez <halpuche@cocobongo.com>
2024-07-12 12:47:14 -0500

    se agrego archivo .env para proteger las credewnciales de s3

---
+++ src/s3-credentials.ts
@@ -0,0 +1,13 @@
+
+export function s3Credentials() {
+
+    const s3Credentials = {
+        region: process.env.AWS_REGION, 
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, 
+        }
+    }
+
+    return s3Credentials
+}
